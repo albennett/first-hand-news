139e5a1a5050b8556d1491f9a0d95ba8e65ce6a9
Amy Lee <amyleeannebennett@gmail.com>
2015-12-10 15:45:20 -0600

    adding boards

--- app/controllers/HomeCtrl.js
+++ app/controllers/HomeCtrl.js
@@ -8,6 +8,7 @@ app.controller("HomeCtrl", ["$scope", "$location", "$firebaseObject", "$firebase
   var auth;
   $scope.categoryTitle = "";
   $scope.sendCategory;
+  $scope.selectedCategory="";
 
    //Logout, unauthorizes the user and logs them back out
     $scope.logout = function(){
@@ -19,7 +20,7 @@ app.controller("HomeCtrl", ["$scope", "$location", "$firebaseObject", "$firebase
   $scope.uidRefData = $firebaseObject(uidRef);
   $scope.uidRefData.$loaded() 
     .then(function(){
-      console.log("scope.data", $scope.uidRefData);
+      console.log("scope.uidrefdata", $scope.uidRefData);
   //once uidRefData is loaded, then if there's a value, user is already saved and userExists changes to true
       if ($scope.uidRefData.$value !== null) {
         console.log('user already saved');
@@ -37,14 +38,18 @@ app.controller("HomeCtrl", ["$scope", "$location", "$firebaseObject", "$firebase
 
 
     //firebase ref for categories
-    var categoriesRef = new Firebase("https://first-hand-accounts.firebaseio.com/categories/");
+    var categoriesRef = new Firebase("https://first-hand-accounts.firebaseio.com/categories");
     $scope.allCategories = $firebaseArray(categoriesRef);
+    console.log("scope.allCategories", $scope.allCategories);
 
     $scope.sendCategory = function() {
       console.log("$scope.categorytitle", $scope.categoryTitle);
-      categoriesRef.push({
+      $scope.allCategories.$add({
         userId: authData.uid,
         title: $scope.categoryTitle   
+      }).then(function(createdcat){
+        var createdCategoryId = createdcat.key();
+        console.log("createdCategoryId", createdCategoryId);
       }) 
     }
     
