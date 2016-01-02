c324b7776f8be31b25b62cae8767101990fd0f18
Amy Lee <amyleeannebennett@gmail.com>
2016-01-01 20:19:16 -0600

    routed story author path correctly

--- app/controllers/YourStoriesCtrl.js
+++ app/controllers/YourStoriesCtrl.js
@@ -3,47 +3,56 @@ app.controller("YourStoriesCtrl", ["$scope", "$location", "$firebaseObject", "$f
 
   	var ref = new Firebase("https://first-hand-accounts.firebaseio.com/");
   	$scope.authData = $firebaseAuth(ref).$getAuth();
-  	var storyRef = new Firebase("https://first-hand-accounts.firebaseio.com/stories");
   	console.log("$scope.authData", $scope.authData);
+  	var storyRef = new Firebase("https://first-hand-accounts.firebaseio.com/stories");
   	var userId = $routeParams.user_id;
   	var refUser = new Firebase("https://first-hand-accounts.firebaseio.com/users/" + userId);
   	$scope.refUserArray = $firebaseArray(refUser);
   	$scope.anonymous = false;
   	$scope.signedIn = false;
+  	$scope.userLoggedIn = false;
 //checks to see if users uid is equal to userId(users personal stories view)
+  	
+// $scope.authData.$loaded()
+//     .then(function(){
+
   	if ($scope.authData) {
   		if ($scope.authData.uid === userId){
 	  			$scope.signedIn = true;
 	  	}
-		}
+	}
+	
 //checks to see if user is logged in
-	  	$scope.userLoggedIn = function() {
-	    if ($scope.authData)  {
-	      return true;
-	    	}
-	  	};
+  	$scope.userLoggedIn = function() {
+    if ($scope.authData)  {
+      return true;
+    	}
+  	};
 	  //if user is logged in, and viewing own stories, then can see own stories with Anonymous and can delete stories
-	  	if ($scope.authData){
-	  		$scope.canYouSeeAnonymous = function() {
-	  			if ($scope.authData.uid === userId){
-	  				return true;
-	  			}
-	  		}
+  	if ($scope.authData){
+  		$scope.canYouSeeAnonymous = function() {
+  			if ($scope.authData.uid === userId){
+  				return true;
+  			}
+  		}
+  	}
 
-		  	$scope.canYouDelete = function(){
-			  	if ($scope.authData.uid === userId){
-			  		return true;
-			  	}
-		  	}	
+  	$scope.canYouDelete = function(){
+	  	if ($scope.authData.uid === userId){
+	  		return true;
 	  	}
+  	}	
 
   	$scope.logout = function(){
 	    $firebaseAuth(ref).$unauth();
 	    console.log("logged out");
   	};
+  	// });
 
 //show if story with user who created it equals the userId in routeparams
+
     var query = storyRef.orderByChild("User").equalTo(userId);
+    console.log("query", query);
     $scope.yourStoriesArray = $firebaseArray(query);
     
     $scope.yourStoriesArray.$loaded()
